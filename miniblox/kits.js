3c5b9ba54ca2e3d24e384fa4326556d8415be178
Roblox-Thot <67937010+Roblox-Thot@users.noreply.github.com>
2024-10-11 12:32:44 -0400

    WIP

---
+++ miniblox/kits.js
@@ -0,0 +1,9 @@
+const mcData = require('minecraft-data')("1.8.9");
+let KITPVP_KITS = {
+	// knight: new KitKnight,
+	// archer: new KitArcher,
+	// tank: new KitTank
+};
+
+
+module.exports = KITPVP_KITS
