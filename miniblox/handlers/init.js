2bebdcbe668e3a18baf1edf50c5b5906874dc996
6x68 <249649284+fun-fact-is-that-everything-after-the-plus-sign-is-useless@users.noreply.github.com>
2026-07-17 21:33:16 -0500

    feat: packet interop stuff thingy

--- miniblox/handlers/init.js
+++ miniblox/handlers/init.js
@@ -2,5 +2,6 @@ exports.entity = require('./impl/entity.js');
 exports.world = require('./impl/world.js');
 exports.tablist = require('./impl/tablist.js');
 exports.gui = require('./impl/gui.js');
+exports.interop = require('./impl/interop.js');
 exports.scoreboard = require('./impl/scoreboard.js');
 exports.misc = require('./impl/misc.js');
