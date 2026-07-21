f1e797357672c8e003bb4694270be700989d2f33
6x68 <249649284+fun-fact-is-that-everything-after-the-plus-sign-is-useless@users.noreply.github.com>
2026-07-21 09:47:18 -0500

    refactor(base/packets/s2c): let -> const
    the biggest change of the century trust

--- base/packets/s2c/player.js
+++ base/packets/s2c/player.js
@@ -6,10 +6,10 @@ const { writeString } = require('../buf_utils');
  * @returns {Buffer} packet to send over `layer:player`
  */
 module.exports = function writePlayer(remoteName, remoteUUID) {
-		const len = remoteName.length + 1 + remoteUUID.length + 1;
+	const len = remoteName.length + 1 + remoteUUID.length + 1;
 
-		const data = Buffer.alloc(len);
-		let off = writeString(data, 0, remoteName);
-		writeString(data, off, remoteUUID);
-		return data;
+	const data = Buffer.alloc(len);
+	const off = writeString(data, 0, remoteName);
+	writeString(data, off, remoteUUID);
+	return data;
 }
